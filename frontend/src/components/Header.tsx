9847f6aacc3f7bdb6e7d2c03628070cab41c962d
fepu08 <ferencbesenyei@protonmail.com>
2023-11-03 22:04:51 +0100

    added Header & Footer Components

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>ReadyShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='/cart'>
+                <FaShoppingCart />
+                &nbsp;Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <FaShoppingCart />
+                &nbsp;Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
