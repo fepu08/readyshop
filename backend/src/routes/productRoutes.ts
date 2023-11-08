e77cd395617c8d0f5bcff80370a6e366bf95619f
fepu08 <ferencbesenyei@protonmail.com>
2023-11-08 15:21:39 +0100

    fixed product routes url

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -2,25 +2,26 @@ import express from 'express';
 import asyncHandler from '../middlewares/asyncHandler';
 import Product from '../schemas/productSchema';
 
-const router = express.Router();
+const productRouter = express.Router();
 
-router.get(
-  '/api/products',
+productRouter.get(
+  '/',
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
   }),
 );
 
-router.get(
-  '/api/products/:id',
+productRouter.get(
+  '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
+      res.status(404);
+      throw new Error('Resource not found');
     }
     res.json(product);
   }),
 );
 
-export default router;
+export default productRouter;
